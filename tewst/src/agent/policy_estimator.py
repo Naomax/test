c3eba8368d311f249f3855e567a256d31cde945f
Naomax <66714115+Naomax@users.noreply.github.com>
2024-01-15 08:11:46 +0900

    Add files via upload

--- tewst/src/agent/policy_estimator.py
+++ tewst/src/agent/policy_estimator.py
@@ -1,7 +1,8 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense
-from tensorflow.keras.models import Model
+import param
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, GlobalAveragePooling2D, MaxPooling2D, Activation
+from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras import backend as K
 
 tf.compat.v1.disable_eager_execution()
@@ -12,20 +13,26 @@ class PolicyEstimator:
                  dim_action,
                  leaning_rate=1e-3):
         self.dim_state = dim_state
+        self.height = dim_state[0]
+        self.width = dim_state[1]
         self.dim_action = dim_action
         self.leaning_rate = leaning_rate
         self.build_network()
         tf.compat.v1.disable_eager_execution()
         self.compile()
 
-    def build_network(self):
+    def build_network_old(self):
         nb_dense_1 = self.dim_state * 10
         nb_dense_3 = self.dim_action * 10
-        nb_dense_2 = int(np.sqrt(nb_dense_1 *
-                                 nb_dense_3))
+        #nb_dense_2 = int(np.sqrt(nb_dense_1 *
+        #                         nb_dense_3))
+        
 
-        l_input = Input(shape=(self.dim_state,),
-                        name='input_state')
+        print("dimstate ", self.dim_state)
+        print("shape", (self.height, self.width, 2))
+        l_input = Conv2D(64, (3, 3), activation = 'relu', input_shape = self.dim_state)
+        #l_input = Input(shape=(self.dim_state,),
+        #               name='input_state')
         l_dense_1 = Dense(nb_dense_1,
                           activation='tanh',
                           name='hidden_1')(l_input)
@@ -46,12 +53,24 @@ class PolicyEstimator:
                            outputs=[l_mu, l_log_var])
         self.model.summary()
 
+    def build_network(self):
+        num_classes = 1
+
+        input_layer = Input(shape = (self.height, self.width, 2))
+        l_dense_1 = Conv2D(32, kernel_size = (3, 3), activation = 'relu')(input_layer)
+        l_dense_2 = Conv2D(64, kernel_size = (3, 3), activation = 'relu')(input_layer)
+        l_dense_3 = MaxPooling2D(pool_size=(2, 2))(l_dense_2)
+        l_dense_4 = GlobalAveragePooling2D()(l_dense_3)  # Flatten の代わりに GlobalAveragePooling2D を使用
+        l_mu = Dense(num_classes, activation = 'tanh')(l_dense_4)
+        l_log_var = Dense(num_classes, activation = 'tanh')(l_dense_4)
+        self.model = Model(inputs = [input_layer], outputs = [l_mu, l_log_var])
+        self.model.summary()
+
     def compile(self):
-        tf.compat.v1.disable_eager_execution()
         self.state = tf.compat.v1.placeholder(
-            tf.float32, shape=(None, self.dim_state))
+            tf.float32, shape=(None, self.height, self.width, 2))
         self.action = tf.compat.v1.placeholder(
-            tf.float32, shape=(None, self.dim_action))
+            tf.float32, shape=(None, 1))
         self.advantage = tf.compat.v1.placeholder(tf.float32,
                                         shape=(None, 1))
 
@@ -75,17 +94,23 @@ class PolicyEstimator:
     def predict(self, sess, state):
         mu, log_var = sess.run([self.mu, self.log_var],
                                {self.state: [state]})
-        mu, log_var = mu[0], log_var[0]
-        var = np.exp(log_var)
-        action = np.random.normal(loc=mu,
-                                  scale=np.sqrt(var))
+        return np.random.normal(loc = mu[0][0], scale = np.sqrt(np.exp(log_var[0][0])))
+        print("mu : ", mu)
+        print("log_var : ", log_var)
+        print("action : ", action)
+        #mu, log_var = mu[0], log_var[0]
+        #var = np.exp(log_var)
+        #print("mu, log_var, var", mu, log_var, var)
+        #action = np.random.normal(loc=mu,
+        #                          scale=np.sqrt(var))
         return action
 
     def update(self, sess, state, action, advantage):
         feed_dict = {
             self.state: state,
-            self.action: action,
-            self.advantage: advantage
+            self.action: np.expand_dims(action, axis=1),
+            #self.advantage: np.expand_dims(advantage, axis = 1),
+            self.advantage: np.expand_dims(np.squeeze(advantage), axis=1),
         }
         _, loss = sess.run([self.minimize, self.loss],
                            feed_dict)
--- tewst/src/agent/value_estimator.py
+++ tewst/src/agent/value_estimator.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, GlobalAveragePooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 
@@ -11,10 +11,12 @@ class ValueEstimator:
     def __init__(self, dim_state, leaning_rate=1e-3):
         self.dim_state = dim_state
         self.leaning_rate = leaning_rate
+        self.height = dim_state[0]
+        self.width = dim_state[1]
         self.build_network()
         self.compile()
 
-    def build_network(self):
+    def build_network_old(self):
         nb_dense_1 = self.dim_state * 10
         nb_dense_3 = 5
         nb_dense_2 = int(np.sqrt(nb_dense_1 *
@@ -38,9 +40,22 @@ class ValueEstimator:
                            outputs=[l_vs])
         self.model.summary()
 
+    def build_network(self):
+        num_classes = 10
+
+        input_layer = Input(shape = (self.height, self.width, 2))
+        l_dense_1 = Conv2D(32, kernel_size = (3, 3), activation = 'relu')(input_layer)
+        l_dense_2 = Conv2D(64, kernel_size = (3, 3), activation = 'relu')(input_layer)
+        l_dense_3 = MaxPooling2D(pool_size=(2, 2))(l_dense_2)
+        l_dense_4 = GlobalAveragePooling2D()(l_dense_3)  # Flatten の代わりに GlobalAveragePooling2D を使用
+        l_mu = Dense(num_classes, activation = 'relu')(l_dense_4)
+        l_log_var = Dense(num_classes, activation = 'softmax')(l_dense_4)
+        self.model = Model(inputs = [input_layer], outputs = [l_mu, l_log_var])
+        self.model.summary()
+
     def compile(self):
         self.state = tf.compat.v1.placeholder(
-            tf.float32, shape=(None, self.dim_state))
+            tf.float32, shape=(None, self.height, self.width, 2))
         self.target = tf.compat.v1.placeholder(tf.float32,
                                      shape=(None, 1))
 
